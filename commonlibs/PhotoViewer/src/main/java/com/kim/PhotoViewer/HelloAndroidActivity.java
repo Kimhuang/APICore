09630edf5f7fe44ba659a65e092632fb760d90fb
黄震 <zhenh@shenzhen.lifeix.com>
2014-07-15 16:16:01 +0800

    导入PhotoViewer图片展示控件

---
+++ commonlibs/PhotoViewer/src/main/java/com/kim/PhotoViewer/HelloAndroidActivity.java
@@ -0,0 +1,14 @@
+package com.kim.PhotoViewer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelloAndroidActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
+
